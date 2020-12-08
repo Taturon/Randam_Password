656a3089b535474e86e4428de1220c0163154161
Taturon <taturon5511@gmail.com>
2020-12-08 19:20:48 +0900

    [Add] ランダムなパスワードを生成する関数を定義し結果をブラウザ上に出力

--- randam_password.php
+++ randam_password.php
@@ -1,3 +1,20 @@
+<?php
+
+/**
+ * ランダム文字列生成 (英数字)
+ * $length 生成する文字数
+ */
+function makeRandStr($length) {
+	$str = array_merge(range('a', 'z'), range('0', '9'), range('A', 'Z'), ['@', '#', '$', '%', '&', '(', ')', '=', '~', '|', '[', ']', '{', '}', '?']);
+	$r_str = null;
+	for ($i = 0; $i < $length; $i++) {
+		$r_str .= $str[rand(0, count($str) - 1)];
+	}
+	return $r_str;
+}
+$pass = makeRandStr(8);
+
+?>
 <!DOCTYPE html>
 <html lang="ja">
 	<head>
@@ -7,5 +24,6 @@
 		<title>ランダムパスワード</title>
 	</head>
 	<body>
+		<p>パスワード案<br>&thinsp;<?= $pass ?></p>
 	</body>
 </html>
